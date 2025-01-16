aacfc284ad5c96367f5d90533468b2bd76cf8be2
Tash <tasm86688@gmail.com>
2025-01-16 20:38:27 +0300

    feature/implement-supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:vaultiq/src/common/di/injector.dart';
 import 'package:vaultiq/src/features/app/pages/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load();
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseKey = dotenv.env['SUPABASE_KEY'];
+
+  if (supabaseUrl == null || supabaseKey == null) {
+    throw Exception('Supabase URL or Key is missing');
+  }
+
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
+
   injectDependencies();
 
   runApp(const App());
