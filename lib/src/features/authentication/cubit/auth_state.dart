c03af159d545eeedfb3a86f5cb6019f5c74eb246
Tash <tasm86688@gmail.com>
2025-01-16 21:30:59 +0300

    feature/auth

---
+++ lib/src/features/authentication/cubit/auth_state.dart
@@ -0,0 +1,12 @@
+part of 'auth_cubit.dart';
+
+class AuthState extends Equatable {
+  @override
+  List<Object?> get props => [];
+
+  const AuthState();
+
+  AuthState copyWith() {
+    return const AuthState();
+  }
+}
