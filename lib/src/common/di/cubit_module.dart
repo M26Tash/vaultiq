281d76d150d4fd5338df27d9dc55f272b1caa26e
Tash <tasm86688@gmail.com>
2025-02-05 20:39:01 +0300

    all-pages-ui: fixed analyzer

--- lib/src/common/di/cubit_module.dart
+++ lib/src/common/di/cubit_module.dart
@@ -1,11 +1,11 @@
 import 'package:vaultiq/src/common/di/injector.dart';
 import 'package:vaultiq/src/common/shared_cubits/navigation_panel_cubit/navigation_panel_cubit.dart';
 import 'package:vaultiq/src/common/shared_cubits/theme_cubit/theme_shared_cubit.dart';
+import 'package:vaultiq/src/features/add_transaction_page/cubit/add_transaction_cubit.dart';
 import 'package:vaultiq/src/features/app/cubit/app_cubit.dart';
 import 'package:vaultiq/src/features/authentication_page/cubit/auth_cubit.dart';
 import 'package:vaultiq/src/features/default_currency_page/cubit/default_currency_cubit.dart';
 import 'package:vaultiq/src/features/forgot_password_page/cubit/forgot_password_cubit.dart';
-import 'package:vaultiq/src/features/add_transaction_page/cubit/add_transaction_cubit.dart';
 import 'package:vaultiq/src/features/main/cubits/wallet_cubit/wallet_cubit.dart';
 import 'package:vaultiq/src/features/otp_page/cubit/otp_cubit.dart';
 
